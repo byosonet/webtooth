3a9b312da29f7090b67dd95db01f00dbfaba9694
byosonet <byosonet@gmail.com>
2020-12-28 12:37:19 -0600

    custom report pdf

--- webtooth/settings.py
+++ webtooth/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 DECIMAL_SEPARATOR = '.'
 DATE_FORMAT ='d M y'
-DATETIME_FORMAT='d M y H: i a'
+DATETIME_FORMAT='d/M/y H:i a'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
