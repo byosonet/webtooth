a046c545a78b414e56a62867d2d93134d16544b6
byosonet <byosonet@gmail.com>
2021-02-08 20:35:39 -0600

    add calc age for patient

--- apppatients/forms.py
+++ apppatients/forms.py
@@ -31,7 +31,7 @@ class PatientForm(forms.ModelForm):
 	sexo = forms.CharField(required=False, max_length=50, widget=forms.Select(attrs=selectCSS('Sexo'), choices=OPTIONS_SEXO))
 	ocupacion = forms.CharField(required=False, max_length=50, widget=forms.TextInput(attrs=inputCSS('Ocupación')))
 	fechaNacimiento = forms.DateTimeField(input_formats=DATE_INPUT_FORMAT, required=False, widget=forms.DateInput(
-		format=DATE_INPUT_SHOW, attrs=inputCSS('Fecha de nacimiento')))
+		format=DATE_INPUT_SHOW, attrs=inputCSS('Fecha nacimiento')))
 	fechaUpdate = forms.DateTimeField(input_formats=DATETIME_INPUT_FORMAT, required=False, disabled=True,
                                    widget=forms.DateInput(format=DATETIME_INPUT_SHOW, attrs=inputReadOnly('Fecha de actualizacón')))
 
