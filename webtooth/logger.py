16284145d82e2811cd073568254e9088452b01fa
byosonet <byosonet@gmail.com>
2021-03-10 22:28:45 -0600

    filter error log

--- webtooth/logger.py
+++ webtooth/logger.py
@@ -1,5 +1,11 @@
 from django.conf import settings
 
+def filter_record_error(record):
+    if record.levelname == settings.FILTER_FILE_ERROR:
+        return True
+    else:
+        return False
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -10,8 +16,9 @@ LOGGING = {
         },
     },
     'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
+        'filter_record': {
+            '()': 'django.utils.log.CallbackFilter',
+            'callback': filter_record_error
         },
     },
     'handlers': {
@@ -20,6 +27,13 @@ LOGGING = {
             'filters': [],
             'class': 'logging.FileHandler',
             'filename': settings.PATH_LOGS+'/webtooth.log',
+            'formatter': 'backend'
+        },
+        'file_error': {
+            'level': settings.LEVEL_LOG,
+            'filters': ['filter_record'],
+            'class': 'logging.FileHandler',
+            'filename': settings.PATH_LOGS+'/webtooth-error.log',
             'formatter': 'backend'
         },
         'console': {
@@ -31,151 +45,151 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'django.request': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'webtooth.views': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'apppatients.views': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'appfiles.views': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'appnavigations.views': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'appimports.views': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'apptasks.views': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'apprecipes.views': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'appproperties.views': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'appadmin.views': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'appinventories.views': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'webtooth.config': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'apppatients.config': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'appfiles.config': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'appimports.config': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'apptasks.config': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'apprecipes.config': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'appadmin.config': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'tasks.config': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'webtooth.signals': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'webtooth.decorators': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'django.db.backends': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
          'django.template': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
         },
         'django.server': {
-            'handlers': ['file', 'console'],
+            'handlers': ['file', 'console','file_error'],
             'level': settings.LEVEL_LOG,
             'propagate': settings.PROPAGATE_LOG,
             'formatter': 'backend',
