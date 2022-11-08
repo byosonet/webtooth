59dd5c04134a31ca1e3dd7772e254a8397b6273f
byosonet <byosonet@gmail.com>
2022-11-07 22:29:18 -0600

    fix to media

--- webtooth/urls.py
+++ webtooth/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 from webtooth.views import homeView, error404, notHasPermission, color
 
 admin.site.site_header = 'Webtooth'
@@ -56,6 +58,7 @@ urlpatterns = [
 
     #UrlsNotHasPermissions
     path('permission/required/', notHasPermission, name="notpermission"),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #Excepciones when Debug is False
